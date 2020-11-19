979a3ff7a0d5c097a03bdb827943a446d7a7321b
Sam Robbins <samrobbinsgb@gmail.com>
2020-11-19 18:54:47 +0000

    Set up site

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./themes/tech-site/layouts/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
